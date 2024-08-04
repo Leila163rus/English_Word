dfe65c6cacc81415099e682b0c757874b6427af9
Пользователь <lilu163@mail.ru>
2024-08-04 22:58:01 +0400

    add

--- src/components/script/setZoom.js
+++ src/components/script/setZoom.js
@@ -1,6 +1,6 @@
 export function setZoom() {
-  if (window.matchMedia('(min-width: 360px) and (max-width: 1280px)').matches) {
-  document.body.style.zoom = "40%";
+  if (window.matchMedia('(min-width: 320px) and (max-width: 1280px)').matches) {
+  document.body.style.zoom = "50%";
   } else {
   document.body.style.zoom = "100%";
   }
